6e3e79288232da94a5ad9badd7a69131264654fb
Rafael Martinelli <martinelli@puc-rio.br>
2022-10-03 14:42:01 -0300

    Including literature constraint. Not working yet.

--- test/Main.jl
+++ test/Main.jl
@@ -6,7 +6,7 @@ using Gurobi
 using Printf
 
 function main()
-    data = loadMDVSP("m4n500s0")
+    data = loadMDVSP("m4n500s1")
 
     V = data.vertices
     D = data.depots
@@ -20,23 +20,30 @@ function main()
 
     @objective(model, Min, sum(c[i, j]x[i, j, d] for i in V, j in V, d in D if existsArc(D, c, i, j, d)) + sum(c[i, i] for i in T))
 
+    @constraint(model, veh[d in D], sum(x[d, j, d] for j in T if existsArc(D, c, d, j, d)) <= m[d])
     @constraint(model, from[i in T], sum(x[i, j, d] for j in V, d in D if existsArc(D, c, i, j, d)) == 1)
     @constraint(model, to[j in T], sum(x[i, j, d] for i in V, d in D if existsArc(D, c, i, j, d)) == 1)
     @constraint(model, dep[d in D], sum(x[d, j, d] for j in T if existsArc(D, c, d, j, d)) == 
         sum(x[i, d, d] for i in T if existsArc(D, c, i, d, d)))
-    @constraint(model, veh[d in D], sum(x[d, j, d] for j in T if existsArc(D, c, d, j, d)) <= m[d])
+    # @constraint(model, flow[i in T, d in D], sum(x[i, j, d] for j in V if existsArc(D, c, i, j, d)) ==
+    #     sum(x[j, i, d] for j in V if existsArc(D, c, j, i, d)))
 
-    write_to_file(model, "model.lp")
+    # write_to_file(model, "model.lp")
     optimize!(model)
     println(termination_status(model))
 
-    @printf("z = %.0f\n", objective_value(model))
+    @printf("UB = %.3f\n", objective_value(model))
+    @printf("LB = %.3f\n", objective_bound(model))
 
     return data
 end
 
 function existsArc(D::Vector{Int64}, c::Matrix{Int64}, i::Int64, j::Int64, d::Int64)
-    return c[i, j] != -1 && (i > length(D) || i == d) && (j > length(D) || j == d)
+    return c[i, j] != -1 && (!isDepot(D, i) || i == d) && (!isDepot(D, j) || j == d)
+end
+
+function isDepot(D::Vector{Int64}, i::Int64)
+    return i <= length(D)
 end
 
 data = main()
